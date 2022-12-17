0141c7585ec66f7abe47eac99aba5d649b87f177
HesaraYasaswin <120818886+HesaraYasaswin@users.noreply.github.com>
2022-12-17 23:47:41 +0530

    Update challenge2.java

--- challenge2.java
+++ challenge2.java
@@ -5,8 +5,8 @@ class challenge2
 
 		public boolean valid(String s)
 		{
-			int length=s.length();
-			char [] array=s.toCharArray();
+	            int length=s.length();
+		    char [] array=s.toCharArray();
 		    if(length==0) return true; 
 		    Stack<Character> stack = new Stack();
 		    for(int i=0; i<length; i++)
